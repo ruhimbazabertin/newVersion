338e6d8bd05db673ec7a4d9dee7dac1963297bea
bertin <ruhimbazab@gmail.com>
2020-03-08 17:25:48 +0200

    correction of json
    added json data
    
    [finishes #171686569]

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -4,11 +4,11 @@
     <b-card 
             :title="name"
             img-src="https://picsum.photos/600/300/?image=25"
-            img-alt="Job-info"
+            img-alt=""
             img-top
             tag="article"
             style="max-width:20rem"
-            class="mb-2"
+            class="mb-2 ml-4"
             footer="Card Footer">
             
       <b-card-text>
